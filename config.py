f468c6c9bc955783a3605cb2841536d4c34374c1
3n0rm048 <150841987+3n0rm048@users.noreply.github.com>
2024-02-22 14:51:54 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 api_id = 29595682
 api_hash = "acfc97cf83b77467dcc79ac5eea4f7f2"
-bot_token = "6506200260:AAH_eoykc5JZJ0JjcaUqq_WOPMgNI0JlANk"
-auth_users = "6674083654, 1318247204"
+bot_token = "7010030739:AAHUJPuV2LZMJwzQR3EhcsUUvyg9tGumO8o"
+auth_users = "5223390523, 1318247204"
 sudo_users = [int(num) for num in auth_users.split(",")]
-osowner_users = "6674083654"
+osowner_users = "5223390523"
 owner_users = [int(num) for num in osowner_users.split(",")]
